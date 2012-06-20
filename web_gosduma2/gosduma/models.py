37cf5aa495b1f1f4e95dbab4c200675d03182e38
Sergey Panfilov <sergray@gmail.com>
2012-06-20 14:24:19 +0400

    Skeleton of Django project with gosduma app and python requirements

---
+++ web_gosduma2/gosduma/models.py
@@ -0,0 +1,49 @@
+# coding=utf8
+"""
+Модели для хранения данных, загруженных из API российской госдумы
+
+Терминология выбрана максимально приближеной к виду возвращаемых данных.
+"""
+from django.db import models
+
+
+class Law(models.Model):
+    """Законопроект внесенный в госдуму
+
+    Например результат поиска по законам https://gist.github.com/7c31fae41d86c1d3f8cd
+    """
+    number = models.CharField(max_length=36, db_index=True, unique=True)  # номер законопроекта
+    introduction_date = models.DateField(required=True, db_index=True)  # дата внесения
+    name = models.TextField(required=True)  # название
+    comments = models.TextField(default='')  # комментарии
+    transcript_url = models.URLField()  # адрес транскрипта обсуждения
+    url = models.URLField()  # адрес законопроекта в АСОЗД (Автоматизированной Системе Обеспечения Законодательной Деятельности)
+
+    def __unicode__(self):
+        return self.number
+
+
+class Voting(models.Model):
+    """Результаты голосования депутатов по законопроекту.
+
+    Например http://api.duma.gov.ru/api/transcript/70707-6:
+
+        Владимир Иванович Бессонов также предлагает снять пункты 2 и 3. Ставлю на
+        голосование.
+
+                       РЕЗУЛЬТАТЫ ГОЛОСОВАНИЯ (10 час. 55 мин. 18 сек.)
+        Проголосовало за               87 чел.19,3 %
+        Проголосовало против            1 чел.0,2 %
+        Воздержалось                    0 чел.0,0 %
+        Голосовало                     88 чел.
+        Не голосовало                 362 чел.80,4 %
+        Результат: не принято
+    """
+    law = models.ForeignKey('gosduma.Law')
+    order = models.PositiveIntegerField()  # порядок голосования
+    title = models.TextField()  # повестка голосования
+    decision = models.TextField()  # тестовый результат голосования
+    pros = models.PositiveIntegerField()  # число голосов "за""
+    cons = models.PositiveIntegerField()  # число голосов "против"
+    abstained = models.PositiveIntegerField()  # число воздержавшихся
+    voted = models.PositiveIntegerField()  # число проголосовавших
