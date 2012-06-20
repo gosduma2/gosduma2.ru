6ae1b7b085add91f5a0febeda977edf54874f2f1
Sergey Panfilov <sergray@gmail.com>
2012-06-20 17:20:48 +0400

    Fix path to sqlite3 db. Add db file to ignore list

--- web_gosduma2/web_gosduma2/settings.py
+++ web_gosduma2/web_gosduma2/settings.py
@@ -1,4 +1,6 @@
 # Django settings for web_gosduma2 project.
+import os
+from os.path import abspath, join, dirname
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'gosduma2',                      # Or path to database file if using sqlite3.
+        'NAME': abspath(join(dirname(__file__), '../gosduma2.db')),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -122,6 +124,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'django_extensions',
     'gosduma',
 )
 
